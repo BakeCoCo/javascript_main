380bce368790cc48fe4044947ac1c2e54ea52ec3
BakeCoCo <58055835+BakeCoCo@users.noreply.github.com>
2020-02-19 11:09:23 +0900

    Revert "good"
    This reverts commit 6ec5de8beff7e567b5b93f69cac7fb9ac22b81aa.

--- main/jsfile/bg.js
+++ main/jsfile/bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `/main/images/${imgNumber}.jpg`;
+    image.src = `./images/${imgNumber}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
